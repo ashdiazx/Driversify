452a62e0d74187c93b0f65fdedd587279385922e
Raffy Torres <201508134@ust-ics.mygbiz.com>
2018-10-27 07:32:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/acer/driversify/ReportDriver.java
@@ -0,0 +1,15 @@
+package com.example.acer.driversify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportDriver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_driver);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/acer/driversify/ReportDriver2.java
@@ -0,0 +1,13 @@
+package com.example.acer.driversify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportDriver2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_driver2);
+    }
+}
